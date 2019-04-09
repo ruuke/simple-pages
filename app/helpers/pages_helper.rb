332ea726ea18c7ec047ed4b69736fc7a8f423d3d
ruuke <ruuke@mail.ru>
2019-04-09 18:06:19 +0500

    added pages helpers

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,21 +1,27 @@
 module PagesHelper
-  # генерация дерева страниц и подстраниц с помощью маркированного списка
-  def display_tree(page)
-    tree = "<li>#{link_to page.name, pages_path(page)}</li>"
+  # создание дерева иерархии страниц из ссылок страниц
+  def show_tree(page)
+    tree = "<li>#{link_to page.name, subpage_path(page)}</li>"
     if page.has_children?
       tree += "<ul>"
-      page.children.each do |sub_page|
-        tree += "#{display_tree(sub_page)}"
+      page.children.each do |page|
+        tree += "#{show_tree(page)}"
       end
       tree += "</ul>"
     end
     tree.html_safe
   end
 
-  # создание пути из ссылок вида Page 1 > Page 1.1 > ...
-  def make_path(page)
-    page.ancestors.map do |parent|
-      link_to parent.name, nested_pages_path(parent)
-    end.join(' > ').html_safe
+  #конвертация markdown в html
+  def markdown(page)
+    html_code = page.body
+    # **[строка]** => <b>[строка]<?b>
+    html_code.gsub!(/\*\*(?<bold>.*)\*\*/, '<b>\k<bold></b>')
+    # //[строка]// =>  <i>[строка]</i>
+    html_code.gsub!(/\\\\(?<ital>.*)\\\\/, '<i>\k<ital></i>')
+    # ((name1/name2/name3 [строка])) => <a href="[site]name1/name2/name3">[строка]</a>
+    html_code.gsub!(/\(\((?<link>.*) (?<name>.*)\)\)/, '<a href="/\k<link>">\k<name></a>') # ((name1/name2/name3 [строка])) => name1/name2/name3, строка
+
+    html_code.html_safe
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get '/add', to: 'pages#new'
-  get '/*slug/add', to: 'pages#new', as: :new_nested_page
-  get '/*slug/edit', to: 'pages#edit', as: :edit_nested_page
-  get '/*slug', to: 'pages#show', as: :nested_page
+  get '/*slug/add', to: 'pages#new', as: :new_subpage
+  get '/*slug/edit', to: 'pages#edit', as: :edit_subpage
+  get '/*slug', to: 'pages#show', as: :subpage
   patch '/*slug', to: 'pages#update'
   put '/*slug', to: 'pages#update'
   delete '/*slug', to: 'pages#destroy'
